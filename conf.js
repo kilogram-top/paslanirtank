3feb966e557d0156e1947e25bd557934ea61bab5
Gökhan Mete ERTÜRK <92143124+gokhanmeteerturk@users.noreply.github.com>
2023-05-08 18:36:16 +0300

    committing conf.js

---
+++ conf.js
@@ -0,0 +1,14 @@
+var site_conf = {
+    site_name:'Paslanmaz Tank',
+    about_site:'Paslanmaz tanklar blogu, endÃ¼striyel proseslerde kullanÄ±lan tank, mikseler, reaktÃ¶r vb ekipmanlar' +
+        'iÃ§in ayrÄ±ntÄ±lÄ± bilgiye ulaÅŸmak isteyenler iÃ§in oluÅŸturuldu.<br>' +
+        ' Bu blogda, paslanmaz tanklar baÅŸta olmak Ã¼zere,' +
+        ' endÃ¼striyel proses Ã¼niteleri ile ilgili Ã§eÅŸitli yazÄ±lar yayÄ±nlanmaktadÄ±r.' +
+        ' Reklam iÃ§in aÅŸaÄŸÄ±daki dÃ¼ÄŸmeden iletiÅŸime geÃ§ebilirsiniz.',
+    global_cta_title:'Paslanmaz tank mÄ± arÄ±yorsunuz? Ã–nce bize sorun!',
+    global_cta_desc:'Paslanmaz tank zor iÅŸtir. O kadar ilginÃ§ bir iÅŸtir ki,' +
+        'inceliklerini bilmeyen herkes, bu iÅŸi yapabileceÄŸini sanabilir.<br>' +
+        'Ä°lk Ã¶nce bizimle konuÅŸun, yanlÄ±ÅŸ yÃ¶nlendirmelerle zaman kaybetmeyin!',
+    global_cta_button_text:'Bize UlaÅŸÄ±n',
+    global_cta_link:'https://google.com'
+}
